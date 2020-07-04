0b4bb54afc149bd7d74784a993aae192c5c8ea36
Jefferson <jribeiro300192@gmail.com>
2020-07-04 19:02:14 -0300

    projeto criado

---
+++ src/main/java/com/jefferson/projetogit/ProjetogitApplication.java
@@ -0,0 +1,13 @@
+package com.jefferson.projetogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetogitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jefferson/projetogit/ProjetogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefferson.projetogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
