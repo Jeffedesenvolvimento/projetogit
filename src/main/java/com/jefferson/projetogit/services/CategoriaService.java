7808f45f3a744776c22721735f34d0bed99352fb
Jefferson <jribeiro300192@gmail.com>
2020-07-07 21:44:57 -0300

    Criação Exception para objecto não encontrado

--- src/main/java/com/jefferson/projetogit/services/CategoriaService.java
+++ src/main/java/com/jefferson/projetogit/services/CategoriaService.java
@@ -4,9 +4,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.jefferson.projetogit.domain.Categoria;
 import com.jefferson.projetogit.repository.CategoriaRepository;
+import com.jefferson.projetogit.services.exceptions.ObjectNotFoundException;
 
 
 @Service
@@ -17,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
-		return categoria.orElse(null);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
